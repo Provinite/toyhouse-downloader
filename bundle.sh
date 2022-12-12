e522ee280c7cc9d6b0c7f126a6b49c8ea273b4e4
Collin Driscoll <provinite@gmail.com>
2022-12-12 01:06:01 -0600

    move dist to zips

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 yarn pkg ./build/app.js --no-bytecode --public-packages \"*\" --public --out-dir ./dist
 cp config.example.jsonc ./dist/config.jsonc
+
 mv ./dist/app-linux ./dist/thdownloader-linux-x64
 mv ./dist/app-macos ./dist/thdownloader-macos-x64
 mv ./dist/app-win.exe ./dist/thdownloader-windows-x64.exe
+
+cd dist 
+zip thdownloader-linux-x64.zip thdownloader-linux-x64 config.jsonc
+zip thdownloader-macos-x64.zip thdownloader-macos-x64 config.jsonc
+zip thdownloader-windows-x64.zip thdownloader-windows-x64.exe config.jsonc
+
+rm thdownloader-windows-x64.exe thdownloader-linux-x64 thdownloader-macos-x64 config.jsonc
