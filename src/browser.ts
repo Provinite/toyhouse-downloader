af9eef96bbb70862187cfa125d9582c4ade2a19a
Collin Driscoll <provinite@gmail.com>
2024-06-13 10:56:49 -0500

    fix(login): use appropriate user-agent to allow logins

--- src/browser.ts
+++ src/browser.ts
@@ -28,6 +28,9 @@ export async function startup(): Promise<PreparedBrowser> {
     executablePath: chromium.path,
   });
   const page = await browser.newPage();
+  await page.setUserAgent(
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36"
+  );
   // attach debug logging
   page.on("domcontentloaded", () => logger.debug(`Navigated to ${page.url()}`));
   const cachedCookies = await browserCookies.get();
